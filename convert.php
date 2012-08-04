0155f7d5e335b9777ffe0afd87c6c30f297faea8
Alick Zhao <alick@helium>
2012-08-04 10:06:28 +0800

    improve handling upload error

--- convert.php
+++ convert.php
@@ -31,6 +31,35 @@ function strenc_tolocal($str) {
 function strenc_fromlocal($str) {
     return iconv('GBK', 'UTF-8', $str);
 }
+
+// Make empty upload an error.
+// Credit goes to
+// http://www.php.net/manual/en/features.file-upload.errors.php#107295
+define("UPLOAD_ERR_EMPTY", 5);
+if($_FILES['upload']['size'] == 0 && $_FILES['upload']['error'] == 0) {
+    $_FILES['upload']['error'] = 5;
+}
+$upload_errors = array(
+    UPLOAD_ERR_OK        => "No errors.",
+    UPLOAD_ERR_INI_SIZE    => "Larger than upload_max_filesize.",
+    UPLOAD_ERR_FORM_SIZE    => "Larger than form MAX_FILE_SIZE.",
+    UPLOAD_ERR_PARTIAL    => "Partial upload.",
+    UPLOAD_ERR_NO_FILE        => "No file.",
+    UPLOAD_ERR_NO_TMP_DIR    => "No temporary directory.",
+    UPLOAD_ERR_CANT_WRITE    => "Can't write to disk.",
+    UPLOAD_ERR_EXTENSION     => "File upload stopped by extension.",
+    UPLOAD_ERR_EMPTY        => "File is empty." // add this to avoid an offset
+);
+
+$err = $_FILES['upload']['error'];
+if ($err !== 0) {
+    echo $error_page_html_head;
+    echo '<p>文件上传失败呃</p>' . "\n";
+    echo '<p>具体原因：' . $upload_errors[$err] . '</p>' . "\n";
+    echo $error_page_html_tail;
+    exit;
+}
+
 // Use system locale settings.
 // On Simplified Chinese Windows it's CP936.
 $locale = setlocale(LC_ALL, '');
@@ -53,6 +82,7 @@ $accepted_file_types = array(
 //    'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
 //    'xlsx' => 'application/vnd.openxmlformats-officedocument.spreahsheetml.sheet',
 );
+
 // NOTE: function mime_content_type is deprecated since PHP 5.3
 // and it is undefined if PHP is not compiled with the option mime-magic
 //var_dump(mime_content_type($filename_tmp));
@@ -64,14 +94,14 @@ if (file_exists($file_bin)) {
     $command_str = $file_bin . ' -b --mime-type ' .
          $filename_tmp_posix . ' 2>&1';
     $file_mime_type = exec($command_str);
-    error_log('$file_mime_type: ' . $file_mime_type);
-    $file_valid = (array_key_exists($filename_ext, $accepted_file_types)) &&
-        ($accepted_file_types[$filename_ext] === $file_mime_type);
 } else {
-    // Do simple validation by file extension.
-    $file_valid = (array_key_exists($filename_ext, $accepted_file_types));
+    // Trust mime type info by client side.
+    $file_mime_type = $_FILES['upload']['type'];
 }
 
+error_log('$file_mime_type: ' . $file_mime_type);
+$file_valid = (array_key_exists($filename_ext, $accepted_file_types)) &&
+    ($accepted_file_types[$filename_ext] === $file_mime_type);
 if (! $file_valid) {
     echo $error_page_html_head;
     echo "<p>Invalid file.</p>" . "\n";
@@ -79,55 +109,45 @@ if (! $file_valid) {
     exit;
 }
 
-if ($_FILES['upload']['error'] === UPLOAD_ERR_OK) {
-    $src_filename = preg_replace('/tmp$/', $filename_ext, $filename_tmp);
-    $pdf_filename = preg_replace('/tmp$/', 'pdf', $filename_tmp);
-    error_log('src: ' . strenc_fromlocal($src_filename));
-    error_log('pdf: ' . strenc_fromlocal($pdf_filename));
+$src_filename = preg_replace('/tmp$/', $filename_ext, $filename_tmp);
+$pdf_filename = preg_replace('/tmp$/', 'pdf', $filename_tmp);
+error_log('src: ' . strenc_fromlocal($src_filename));
+error_log('pdf: ' . strenc_fromlocal($pdf_filename));
 
-    $ret = move_uploaded_file($filename_tmp, $src_filename);
-    if (! $ret) {
-        // Handle move_uploaded_file failure.
-        echo $error_page_html_head;
-        echo '<p>噢不好了：移动上传文件失败</p>' . "\n";
-        echo $error_page_html_tail;
-    }
-    try {
-        $wps = new COM("WPS.Application");
-        $doc = $wps->Documents->Open($src_filename);
-        $doc->exportpdf($pdf_filename);
-        $doc->Close();
-        unset($doc, $wps);
-        $fname = 'download.pdf';
-        $fnamestar = rawurlencode(strenc_fromlocal($filename_name)) . '.pdf';
-        // Set headers for downloading.
-        header('Cache-Control: no-cache, must-revalidate');
-        header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-        header('Content-Type: application/pdf');
-        header('Content-Disposition: attachment; filename=' . $fname .
-            "; filename*=utf-8\\''" . $fnamestar);
-        header('Content-Length: ' . filesize($pdf_filename));
-        readfile($pdf_filename);
-        unlink($pdf_filename);
-        unlink($src_filename);
-        exit;
-    } catch (Exception $e) {
-        echo $error_page_html_head;
-        echo '<p>生成PDF失败 -__-</p>' . "\n";
-        echo '<p>异常信息：' . $e->getMessage() . "</p>\n";
-        echo '<pre>';
-        echo $e->getTraceAsString() . "\n";
-        echo '</pre>';
-        echo $error_page_html_tail;
-    }
-} else {
-    /*
-     * TODO
-    switch ($_FILES['upload']['error'])
-    {
-    }
-     */
+// Rename tmp file to have the right extension.
+$ret = move_uploaded_file($filename_tmp, $src_filename);
+if (! $ret) {
+    // Handle move_uploaded_file failure.
     echo $error_page_html_head;
-    echo '<p>文件上传失败呃</p>' . "\n";
+    echo '<p>噢不好了：移动上传文件失败</p>' . "\n";
+    echo $error_page_html_tail;
+}
+// Really do conversion.
+try {
+    $wps = new COM("WPS.Application");
+    $doc = $wps->Documents->Open($src_filename);
+    $doc->exportpdf($pdf_filename);
+    $doc->Close();
+    unset($doc, $wps);
+    $fname = 'download.pdf';
+    $fnamestar = rawurlencode(strenc_fromlocal($filename_name)) . '.pdf';
+    // Set headers for downloading.
+    header('Cache-Control: no-cache, must-revalidate');
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+    header('Content-Type: application/pdf');
+    header('Content-Disposition: attachment; filename=' . $fname .
+        "; filename*=utf-8\\''" . $fnamestar);
+    header('Content-Length: ' . filesize($pdf_filename));
+    readfile($pdf_filename);
+    unlink($pdf_filename);
+    unlink($src_filename);
+    exit;
+} catch (Exception $e) {
+    echo $error_page_html_head;
+    echo '<p>生成PDF失败 -__-</p>' . "\n";
+    echo '<p>异常信息：' . $e->getMessage() . "</p>\n";
+    echo '<pre>';
+    echo $e->getTraceAsString() . "\n";
+    echo '</pre>';
     echo $error_page_html_tail;
 }
