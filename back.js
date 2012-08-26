00c9bd4fa294e5a07a338fab45a564e6a1650583
Alick Zhao <alick@helium>
2012-08-26 21:49:00 +0800

    back.js: change wait time to 3s, add tip info

--- back.js
+++ back.js
@@ -1,5 +1,8 @@
 window.onload = function () {
+    var sec = 3;
+    var msg = sec + ' 秒后自动回到首页';
+    document.body.innerHTML += '<p style="color: orange;">' + msg + '</p>';
     setTimeout(function () {
         window.location.href = 'index.html';
-    }, 5000);
+    }, sec * 1000);
 }
